8107f9df1aca90e94ecaee1b07d5669365034c68
udiittsinghal <59390200+udiittsinghal@users.noreply.github.com>
2025-04-08 14:46:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function Home() {
         >
          <div className="flex justify-center gap-2">
          <img
-  src="/tdo/The day off logo-03.png"
+  src="/tdo-logo1.png"
   alt="Project Image"
   className="w-128 h-80 object-contain"
 />  
